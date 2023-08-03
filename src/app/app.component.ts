6064ae60d11dd3edf9b9f6b9877da5ceca630aed
vathadianil <anilkumarvathadi@gmail.com>
2023-08-03 20:02:43 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  ChangeDetectionStrategy,
+  Component,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-change-detection';
+  name = 'Decoded Frontend';
+  topicName = 'Change Detection in Angular';
+  isVisible = true;
+  creationDate = new Date();
+
+  constructor(private cdr: ChangeDetectorRef) {
+    setTimeout(() => {
+      this.topicName = 'Zone Js Angular';
+      console.log(`Topic Name changed to ${this.topicName}`);
+      this.cdr.markForCheck();
+      this.cdr.detectChanges();
+    }, 3000);
+  }
+
+  getInfo() {
+    return `1M Views`;
+  }
 }
---
+++ src/app/channel/channel.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-channel',
+  templateUrl: './channel.component.html',
+  styleUrls: ['./channel.component.scss'],
+})
+export class ChannelComponent {
+  @Input('name') name = '';
+}
