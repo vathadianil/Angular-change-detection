9eb76bc6651017baa82e6f72f0925cc57ca140b0
vathadianil <anilkumarvathadi@gmail.com>
2023-08-04 14:44:02 +0530

    Favourites info added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ export class AppComponent implements OnInit {
   }
 
   getInfo() {
-    return `1M Views | 10k Likes`;
+    return `1M Views | 10k Likes | 2 Favourites`;
   }
 
   ngOnInit(): void {}
